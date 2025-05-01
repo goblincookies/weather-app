f23dea577a166d544462e97e7262e08fac3b4b3b
goblincookies <goblincookiebox@gmail.com>
2025-05-01 14:29:26 -0400

    adds comments

--- src/assets/modules/ACTIONmanager.js
+++ src/assets/modules/ACTIONmanager.js
@@ -1,7 +1,5 @@
 class Interactions {
-
-    // draggingItems = [];
-
+    
     draggingItem = null;
     boundingBox = null;
     pointerStartX = 0;
@@ -9,55 +7,49 @@ class Interactions {
     offsetX = 0;
     maxSlide = 0;
 
-    // constructor( HTML ) {
-    constructor( ) { //( HTML ) {
-        // HTML.addEventListener( 'mousedown', this.dragStart );
-        // HTML.addEventListener( 'touchstart', this.dragStart );
-
+    constructor( ) { 
         document.addEventListener( 'mouseup', this.dragEnd );
         document.addEventListener('touchend', this.dragEnd);
     };
 
+    // ADD LISTENERS TO ITEM
     watch( HTML ) {
         HTML.addEventListener( 'mousedown', this.dragStart );
         HTML.addEventListener( 'touchstart', this.dragStart );
-        // this.draggingItems.push( HTML );
     };
 
+    // TRIGGERS WHEN A DRAG IS DETECTED
     dragStart = ( e ) => {
-        console.log( `drag start:`);
-        // console.log( e.currentTarget );
 
+        // MAKE SURE IT'S A VIABLE ITEM
+        // THIS IS USEFUL FOR DISABLING
+        // DRAGGING ON CERTAIN ITEMS
         if ( e.currentTarget.classList.contains( 'pan') ) {
             this.draggingItem = e.currentTarget;
             this.boundingBox = e.currentTarget.closest( '.bounding-box' );
             this.pointerStartX = e.clientX || e.touches[0].clientX;
 
+            // IF WE HAVE LOADED VALID ITEMS,
+            // CALC THE MAX MOVEMENT
+            // INSTEAD OF CALCULATING IT EVERY DRAG
+            // THIS COULD BE MOVED TO THE CONSTRUCTOR
+            // AND THEN RECALCULATED ONLY WHEN THE WINDOW IS RESIZED
             if ( this.boundingBox && this.draggingItem ) {
                 const dragBox = this.draggingItem.getBoundingClientRect();
                 const boundBox = this.boundingBox.getBoundingClientRect();
 
                 this.maxSlide = boundBox.width - dragBox.width;
-                console.log( `max slide:${ this.maxSlide }` );    
             };
-            
-            console.log( this.draggingItem );
-            console.log( this.boundingBox );
-
-
         };
 
+        // SET UP DOCUMENT LISTENERS REQUIRED FOR DRAGGIN
         document.addEventListener( 'mousemove', this.drag );
         document.addEventListener( 'touchmove', this.drag, {passive: false});
-        // document.addEventListener("mouseup", dragEnd );
-        // document.addEventListener("touchend", dragEnd);
         document.addEventListener( 'mouseleave', this.dragEnd );
-
-        console.log( 'finished drag start setup' );
     };
 
+    // TRIGGERS WHEN DRAGGING
     drag = ( e ) => {
-        // console.log( 'dragging' );
 
         // PROTECTS AGAINST 'STICKY' DRAGS
         if ( e.buttons < 1 ) {
@@ -71,54 +63,49 @@ class Interactions {
             return;
         };
 
+        // VERIFY AN ITEM HAS BEEN FOUND
         if ( this.draggingItem && this.boundingBox ) {
-            console.log('moving!')
-            // MOVE IT!
-            // console.log( 'dragging' );
+
+            // CALC THE MOUSE MOVEMENT
             const clientX = e.clientX || e.touches[0].clientX;
             let _x = this.offsetX + clientX - this.pointerStartX;
-            console.log( `offsetX: ${this.offsetX}, clientX: ${clientX}, pointerStartX:${ this.pointerStartX}, _x ${ _x }`);
+
+            // CLAMP IT TO THE WINDO
             _x = Math.min( _x, 0 );
             _x = Math.max( _x, this.maxSlide );
 
-            // this.pointerOffsetX = Math.min( this.offsetX + clientX - this.pointerStartX, 0);
-            
             this.pointerOffsetX = _x;
-            // this.pointerOffsetX = this.pointerOffsetX <= 0 ? 0 : 
-            this.draggingItem.style.transform = null;
-
             this.draggingItem.style.transform = `translateX( ${ _x }px)`;
-            console.log( this.draggingItem, _x );
-            // this.offsetX = pointerOffsetX;
         };
     };
 
+    // TRIGGERS WHEN DRAG IS COMPLETE
+    // OR A BUNCH OF WAYS
+    // CLEANS UP LOADED ITEMS, AND MAKES SURE
+    // EVERYTHING IS READY FOR THE NEXT DRAG
     dragEnd = () => {
-        console.log( 'drag end');
-
         this.removeListeners();
         this.cleanup();
     };
 
-    updatePos( HTML, bounding_html, posX ) {
-        // TRANSLATE X SUCH THAT 0 < X < bounding_html.width
-
-    };
-
+    // REMOVES LISTENERS THAT ARE ONLY NEEDED WHILE DRAGGING
     removeListeners() {
         document.removeEventListener( 'mousemove', this.drag );
         document.removeEventListener( 'touchmove', this.drag );
         document.removeEventListener( 'mouseleave', this.dragEnd );
     };
 
+    // GENERAL CLEANUP
+    // UNLOADS ITEMS
     cleanup() {
         this.draggingItem = null;
         this.offsetX = this.pointerOffsetX;
-        // this.pointerOffsetX = 0;
     };
 
+    // RESETS OFFSET AND TRANSFORM
+    // USEFUL WHEN ITEMS SHOULD BE IN THEIR
+    // ORIGINAL STARTING POSITION
     reset( HTML ) {
-        // if ( this.draggingItem ) { this.draggingItem.style.transform = null; }
         HTML.style.transform = null;
         this.draggingItem = null;
         this.offsetX = 0;
